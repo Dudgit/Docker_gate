c167757e1d4a6dd5c8a98db6a1379eb9de5b5472
Bence Dudás <52243953+Dudgit@users.noreply.github.com>
2022-02-28 14:18:56 +0000

    Add files via upload

--- runner.py
+++ runner.py
@@ -1,10 +1,12 @@
 import subprocess
 def main(start_border,end_border,step_size):
     with open("output.log", "a") as output:
-        subprocess.call("docker build -t dude -f docker-image/Dockerfile .", shell=True, stdout=output, stderr=output)
-        for width in range(start_border,end_border,step_size):
-            subprocess.call(f"docker run -v $(pwd)/output/:/output -it dude -dt --simulation water_phantom -wpt {width} --primaries 100000", shell=True, stdout=output, stderr=output)
+        subprocess.call("docker build -t water_phantom_xy -f docker-image/Dockerfile .", shell=True, stdout=output, stderr=output)
+        for bdx in range(start_border,end_border,step_size):
+            for bdy in range(start_border,end_border,step_size):
+                subprocess.call(f"docker run -v /home/bdudas/Bence/D2/output:/output -d --rm -it water_phantom_xy -dt --simulation water_phantom --beam_energy 230 -wpm sphere -wpt 25 -bdx {bdx} -bdy {bdy} --primaries 10000",
+                shell=True, stdout=output, stderr=output)
 
 
 if __name__ == '__main__':
-    main(30,3000,5)
+    main(0,15,5)
