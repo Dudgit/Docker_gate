c87ae649e4078591fdccb0108c3d5748b828e206
Bence Dudás <52243953+Dudgit@users.noreply.github.com>
2022-02-28 15:36:21 +0100

    updated-tested

--- runner.py
+++ runner.py
@@ -4,7 +4,8 @@ def main(start_border,end_border,step_size):
         subprocess.call("docker build -t water_phantom_xy -f docker-image/Dockerfile .", shell=True, stdout=output, stderr=output)
         for bdx in range(start_border,end_border,step_size):
             for bdy in range(start_border,end_border,step_size):
-                subprocess.call(f"docker run -v /home/bdudas/Bence/D2/output:/output -d --rm -it water_phantom_xy -dt --simulation water_phantom --beam_energy 230 -wpm sphere -wpt 25 -bdx {bdx} -bdy {bdy} --primaries 10000",
+                myexecution = "docker run -v /home/bdudas/Bence/D2/output:/output -d --rm -it water_phantom_xy -dt --simulation water_phantom --beam_energy 230 -wpm sphere -wpt 25 -bdx " + str(bdx) + " -bdy " + str(bdy) + " --primaries 10000"
+                subprocess.call(myexecution,
                 shell=True, stdout=output, stderr=output)
 
 
